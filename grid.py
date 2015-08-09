51800336b823bb8470774fb6331a7b42234f8ffa
Gemisheresy <gemisheresy@gmail.com>
2015-08-08 21:51:26 -0400

    Deleted comments

--- grid.py
+++ grid.py
@@ -4,20 +4,25 @@ b = []
 done = False
 point = [0,0]
 def size_grid(x,y):
+    #Makes grid at size
     for i in range(x):
         a.append(0)
     for j in range(y):
         b.append(a[:])
 
 def grid():
+    # Draws grid and clears command line
     cls()
     for i in range(len(b)):
         print b[i]
 def cls():
+    # Clears command line
     os.system(['clear','cls'][os.name == 'nt'])
 def get_current():
+    # Returns command line
     return point
 def move_down():
+    # Moves one down
     if point[0] < len(b )- 1:
         b[point[0]][point[1]] = 0
         b[point[0]+1][point[1]] = 1
@@ -27,6 +32,7 @@ def move_down():
         print "There is no more map that way"
         grid()
 def move_up():
+    # Moves one space up
     if point[0] > 0:
         b[point[0]][point[1]] = 0
         b[point[0]-1][point[1]] = 1
@@ -37,6 +43,7 @@ def move_up():
         grid()
     
 def move_left():
+    # Moves one space left
     if point[1] > 0 :
         b[point[0]][point[1]] = 0
         b[point[0]][point[1]-1] = 1
@@ -47,6 +54,7 @@ def move_left():
         grid()
 
 def move_right():
+    # Moves one space right
     if point[1] < len(a)- 1:
         b[point[0]][point[1]] = 0
         b[point[0]][point[1]+1] = 1
