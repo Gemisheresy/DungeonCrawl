639707d5c58b70bac997b76525dd94d4a2686b98
Gemisheresy <gemisheresy@gmail.com>
2015-08-09 21:49:56 -0400

    Tips from King

--- grid.py
+++ grid.py
@@ -1,20 +1,20 @@
 import os
-a = []
-b = []
+col = []
+row = []
 done = False
 point = [0,0]
 def size_grid(x,y):
     #Makes grid at size
     for i in range(x):
-        a.append(0)
+        col.append(0)
     for j in range(y):
-        b.append(a[:])
+        row.append(col[:])
 
 def grid():
     # Draws grid and clears command line
     cls()
-    for i in range(len(b)):
-        print b[i]
+    for i in range(len(row)):
+        print row[i]
 def cls():
     # Clears command line
     os.system(['clear','cls'][os.name == 'nt'])
@@ -23,51 +23,43 @@ def get_current():
     return point
 def move_down():
     # Moves one down
-    if point[0] < len(b )- 1:
-        b[point[0]][point[1]] = 0
-        b[point[0]+1][point[1]] = 1
+    if point[0] < len(row)- 1:
+        row[point[0]][point[1]] = 0
+        row[point[0]+1][point[1]] = 1
         point[0] = point[0]+1
-        grid()
     else:
         print "There is no more map that way"
-        grid()
 def move_up():
     # Moves one space up
     if point[0] > 0:
-        b[point[0]][point[1]] = 0
-        b[point[0]-1][point[1]] = 1
+        row[point[0]][point[1]] = 0
+        row[point[0]-1][point[1]] = 1
         point[0] = point[0]-1
-        grid()
     else:
         print "There is no more map that way"
-        grid()
     
 def move_left():
     # Moves one space left
     if point[1] > 0 :
-        b[point[0]][point[1]] = 0
-        b[point[0]][point[1]-1] = 1
+        row[point[0]][point[1]] = 0
+        row[point[0]][point[1]-1] = 1
         point[1] = point[1]+-1
-        grid()
     else:
         print "There is no more map that way"
-        grid()
 
 def move_right():
     # Moves one space right
-    if point[1] < len(a)- 1:
-        b[point[0]][point[1]] = 0
-        b[point[0]][point[1]+1] = 1
+    if point[1] < len(col)- 1:
+        row[point[0]][point[1]] = 0
+        row[point[0]][point[1]+1] = 1
         point[1] = point[1]+1
-        grid()
     else:
-        print "There is no more map that way"
-        grid()
+        print "There is no more mcolp thcolt wcoly"
 def start():
-    wide = int(raw_input("How wide do you want the room? "))
+    wide = int(raw_input("How wide do you wcolnt the room? "))
     height= int(raw_input("How High do you want the room? "))
     size_grid(wide,height)
-    b[0][0] = 1
+    row[0][0] = 1
     grid()
     done = False
     while done == False:
@@ -82,5 +74,5 @@ def start():
             move_down()
         elif answer == 'e':
             done = True
-
+        grid()
 start()
