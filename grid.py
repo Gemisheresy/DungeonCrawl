cbb39457740016ff1d6fca232f25bd99a44aeaf5
Gemisheresy <gemisheresy@gmail.com>
2015-08-13 20:14:23 -0400

    removed different move functions and combined into one function. the loop in start now calls the function waiting for raw_input. Also changed the name of grid() to draw_grid() and now it joins into a string. Makes it look better

--- grid.py
+++ grid.py
@@ -6,57 +6,57 @@ point = [0,0]
 def size_grid(x,y):
     #Makes grid at size
     for i in range(x):
-        col.append(0)
+        col.append('0 ')
     for j in range(y):
         row.append(col[:])
 
-def grid():
+def draw_grid():
     # Draws grid and clears command line
     cls()
     for i in range(len(row)):
-        print row[i]
+        print ''.join(row[i])
 def cls():
     # Clears command line
     os.system(['clear','cls'][os.name == 'nt'])
 def get_current():
     # Returns command line
     return point
-def move_down():
-    # Moves one down
-    if point[0] < len(row)- 1:
-        row[point[0]][point[1]] = 0
-        row[point[0]+1][point[1]] = 1
-        point[0] = point[0]+1
-    else:
-        print "There is no more map that way"
-def move_up():
-    # Moves one space up
-    if point[0] > 0:
-        row[point[0]][point[1]] = 0
-        row[point[0]-1][point[1]] = 1
-        point[0] = point[0]-1
-    else:
-        print "There is no more map that way"
-    
-def move_left():
-    # Moves one space left
-    if point[1] > 0 :
-        row[point[0]][point[1]] = 0
-        row[point[0]][point[1]-1] = 1
-        point[1] = point[1]+-1
-    else:
-        print "There is no more map that way"
 
-def move_right():
-    # Moves one space right
-    if point[1] < len(col)- 1:
-        row[point[0]][point[1]] = 0
-        row[point[0]][point[1]+1] = 1
-        point[1] = point[1]+1
-    else:
-        print "There is no more map that way"
-        
-        
+def move(key):
+    if key == 's':
+         # Moves one down
+        if point[0] < len(row)- 1:
+            row[point[0]][point[1]] = '0 '
+            row[point[0]+1][point[1]] = '1 '
+            point[0] = point[0]+1
+        else:
+            print "There is no more map that way"
+    elif key == 'w':
+         # Moves one space up
+        if point[0] > 0:
+            row[point[0]][point[1]] = '0 '
+            row[point[0]-1][point[1]] = '1 '
+            point[0] = point[0]-1
+        else:
+            print "There is no more map that way"
+    elif key == 'a':
+         # Moves one space left
+        if point[1] > 0 :
+            row[point[0]][point[1]] = '0 '
+            row[point[0]][point[1]-1] = '1 '
+            point[1] = point[1]+-1
+        else:
+            print "There is no more map that way"
+    elif key == 'd':
+          # Moves one space right
+        if point[1] < len(col)- 1:
+            row[point[0]][point[1]] = '0 '
+            row[point[0]][point[1]+1] = '1 '
+            point[1] = point[1]+1
+        else:
+            print "There is no more map that way"
+    elif key == 'e':
+            done == True
 
     
 def start():
@@ -73,20 +73,9 @@ def start():
         except ValueError:
             print('That wasnt a number try again')
     size_grid(wide,height)
-    row[0][0] = 1
-    grid()
-    done = False
+    row[0][0] = '1 '
+    draw_grid()
     while done == False:
-        answer=raw_input("where would you like to go?\n")
-        if answer == 'a':
-            move_left()
-        elif answer == 'd':
-            move_right()
-        elif answer == 'w':
-            move_up()
-        elif answer == 's':
-            move_down()
-        elif answer == 'e':
-            done = True
-        grid()
+        move(str(raw_input('Where do you want to go? ')))
+        draw_grid()
 start()
