85c67eb7c703a0ffb6ae3dbbe7790e99ef5195cf
Gemisheresy <Gemisheresy@gmail.com>
2015-08-22 16:54:57 -0400

    Small functions added forconvince of direction control. The idea is that when fighting controls added it will give the hero a direction for checking to see of monster is in front of him of not

--- grid.py
+++ grid.py
@@ -2,7 +2,10 @@ import os
 col = []
 row = []
 done = False
-hero = {'point':[0,0]}
+response = {'phrase' :'', 'last':''}
+hero = {'point':[0,0], 'attack': 1,'health' : 10, 'speed': 1}
+monster = {'point': [0,0],'attack' : 1, 'health' : 10, 'speed' : 1}
+
 def size_grid(x,y):
     #Makes grid at size
     for i in range(x):
@@ -15,12 +18,30 @@ def draw_grid():
     cls()
     for i in range(len(row)):
         print(''.join(row[i]))
+
 def cls():
     # Clears command line
     os.system(['clear','cls'][os.name == 'nt'])
+
 def get_current():
     # Returns command line
     return hero
+
+def change_response_last(key):
+    if key == 'a':
+        response['last'] = 'Left'
+    elif key == 'd':
+        response['last'] = 'Right'
+    elif key == 'w':
+        response['last'] = 'Up'
+    elif key == 's':
+        response['last'] = 'Down'
+
+def change_response_phrase(moved):
+    if moved == True:
+        response['phrase'] = 'The last way you moved was ' + response['last']
+    else:
+        response['phrase'] = 'There is no where to go that way'
 
 def move(key):
     if key == 's':
@@ -29,36 +50,43 @@ def move(key):
             row[hero['point'][0]][hero['point'][1]] = '0 '
             row[hero['point'][0]+1][hero['point'][1]] = '1 '
             hero['point'][0] = hero['point'][0]+1
+            change_response_last(key)
+            change_response_phrase(True)
         else:
-            print("There is no more map that way")
+            change_response_phrase(False)
     elif key == 'w':
          # Moves one space up
         if hero['point'][0] > 0:
             row[hero['point'][0]][hero['point'][1]] = '0 '
             row[hero['point'][0]-1][hero['point'][1]] = '1 '
             hero['point'][0] = hero['point'][0]-1
+            change_response_last(key)
+            change_response_phrase(True)
         else:
-            print("There is no more map that way")
+            change_response_phrase(False)
     elif key == 'a':
          # Moves one space left
         if hero['point'][1] > 0 :
             row[hero['point'][0]][hero['point'][1]] = '0 '
             row[hero['point'][0]][hero['point'][1]-1] = '1 '
             hero['point'][1] = hero['point'][1]+-1
+            change_response_last(key)
+            change_response_phrase(True)
         else:
-            print("There is no more map that way")
+            change_response_phrase(False)
     elif key == 'd':
           # Moves one space right
         if hero['point'][1] < len(col)- 1:
             row[hero['point'][0]][hero['point'][1]] = '0 '
             row[hero['point'][0]][hero['point'][1]+1] = '1 '
             hero['point'][1] = hero['point'][1]+1
+            change_response_last(key)
+            change_response_phrase(True)
         else:
-            print("There is no more map that way")
+            change_response_phrase(False)
     elif key == 'e':
             done = True
 
-    
 def start():
     done = False
     while True:
@@ -84,4 +112,7 @@ def start():
         else:
             move(way)
         draw_grid()
+        print(response['phrase'])
 start()
+
+
