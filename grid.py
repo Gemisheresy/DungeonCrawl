a38a3e111b630ee54606f7db9a2d97ff2083b1fb
Gemisheresy <Gemisheresy@gmail.com>
2015-08-24 22:46:52 -0400

    Cleaned up functions for direction and movement. Put all strings into dictionary for the time being to cean up.Noticed a new bug that if the hero stands in the spot of the defeated monster he is hidden and assigned 0 till moved

--- grid.py
+++ grid.py
@@ -4,9 +4,26 @@ import math
 col = []
 row = []
 done = False
-response = {'phrase' :'', 'last':''}
-hero = {'point':[0,0], 'attack': 1,'health' : 10, 'speed': 1}
-monster = {'point': [0,0],'attack' : 1, 'health' : 10, 'speed' : 1}
+hero = {'point':[0,0],
+        'attack': 1,
+        'health':10,
+        'speed': 1}
+monster = {'point':[0,0],
+           'attack' : 1,
+           'health' : 10,
+           'speed' : 1}
+direction = {'a':'Right',
+             'd':'Left',
+             'w':'Up',
+             's':'Down',
+             'facing':''}
+phrase = {'moved':'You explore this area and find nothing.',
+          'bounds':'There is no where to go that way.',
+          'damaged':'You hit the monster in front of you.',
+          'missed':'You swung and missed.',
+          'defeated':' You defeated the monster, press e to exit the dungeon.',
+          'monster': 'There is a big snarling monster in front of you.',
+          'last':''} 
 
 def size_grid(x,y):
     #Makes grid at size
@@ -28,115 +45,97 @@ def cls():
 def get_current():
     return hero
 
-def change_response_last(key):
-# changes the last direction variable for attacking and phrases
-    if key == 'a':
-        response['last'] = 'Left'
-    elif key == 'd':
-        response['last'] = 'Right'
-    elif key == 'w':
-        response['last'] = 'Up'
-    elif key == 's':
-        response['last'] = 'Down'
+def change_last(key):
+    direction['facing'] = direction.get(key)
+   
+def get_last():
+    return direction['facing']
 
-def change_response_phrase(action):
-# changes the phrase displayed to the user on the screen.
-    if action == 'moved':
-        response['phrase'] = 'The last way you moved was ' + response['last']
-    elif action == 'outofbounds':
-        response['phrase'] = 'There is no where to go that way'
-    elif action == 'damaged':
-        response['phrase'] = 'You hit the monster in front of you'
-    elif action == 'missed':
-        response['phrase'] = 'You swing and miss'
-    elif action == 'defeated':
-        response['phrase'] = 'You defeated the monster. Press e to end the game'
-    elif action == 'monster':
-        response['phrase'] = 'There is a big snarling monster infront of you'
+def change_phrase(action):
+    phrase['last'] = phrase.get(action)
+
 
 def move(key):
 # moves the hero one space down
+    change_last(key)
     if key == 's':
-        change_response_last(key)
         if hero['point'][0] < len(row)- 1:
             if row[hero['point'][0]+1][hero['point'][1]] =='X ':
-                change_response_phrase('monster')
+                change_phrase('monster')
             elif row[hero['point'][0]+1][hero['point'][1]] == '0 ':
                 row[hero['point'][0]][hero['point'][1]] = '0 '
                 row[hero['point'][0]+1][hero['point'][1]] = '1 '
                 hero['point'][0] = hero['point'][0]+1
-                change_response_phrase('moved')
+                change_phrase('moved')
         else:
-                change_response_phrase('outofbounds')
+                change_phrase('bounds')
 # moves the hero one space up
     elif key == 'w':
-        change_response_last(key)
         if hero['point'][0] > 0:
             if row[hero['point'][0]-1][hero['point'][1]] =='X ':
-                change_response_phrase('monster')
+                change_phrase('monster')
             elif row[hero['point'][0]-1][hero['point'][1]] == '0 ':
                 row[hero['point'][0]][hero['point'][1]] = '0 '
                 row[hero['point'][0]-1][hero['point'][1]] = '1 '
                 hero['point'][0] = hero['point'][0]-1
-                change_response_phrase('moved')
+                change_phrase('moved')
         else:
-                change_response_phrase('outofbounds')
+                change_phrase('bounds')
 # moves the hero one space left
     elif key == 'a':
-        change_response_last(key)
         if hero['point'][1] > 0 :
             if row[hero['point'][0]][hero['point'][1]-1] =='X ':
-                change_response_phrase('monster')
+                change_phrase('monster')
             elif row[hero['point'][0]][hero['point'][1]-1] == '0 ':
                 row[hero['point'][0]][hero['point'][1]] = '0 '
                 row[hero['point'][0]][hero['point'][1]-1] = '1 '
                 hero['point'][1] = hero['point'][1]-1
-                change_response_phrase('moved')
+                change_phrase('moved')
         else:
-                change_response_phrase('outofbounds')
+                change_phrase('bounds')
 # moves the hero one space right
     elif key == 'd':
-        change_response_last(key)
         if hero['point'][1] < len(row)- 1:
             if row[hero['point'][0]][hero['point'][1]+1] =='X ':
-                change_response_phrase('monster')
+                change_phrase('monster')
             elif row[hero['point'][0]][hero['point'][1]+1] == '0 ':
                 row[hero['point'][0]][hero['point'][1]] = '0 '
                 row[hero['point'][0]][hero['point'][1]+1] = '1 '
                 hero['point'][1] = hero['point'][1]+1
-                change_response_phrase('moved')
+                change_phrase('moved')
         else:
-                change_response_phrase('outofbounds')
-
+                change_phrase('bounds')
+    
+    
 def attack():
 # checks last direction moved to if monster is in front of hero
-    if response['last'] == 'Left':
+    if direction['facing'] == 'Left':
         if row[hero['point'][0]][hero['point'][1] - 1] == 'X ':
-            change_response_phrase('damaged')
+            change_phrase('damaged')
             monster['health'] -= hero['attack']
         else:
-            change_response_phrase('missed')
+            change_phrase('missed')
             
-    elif response['last'] == 'Down':
+    elif direction['facing'] == 'Down':
         if row[hero['point'][0]+1][hero['point'][1]] == 'X ':
-            change_response_phrase('damaged')
+            change_phrase('damaged')
             monster['health'] -= hero['attack']
         else:
-            change_response_phrase('missed')
+            change_phrase('missed')
             
-    elif response['last'] == 'Up':
+    elif direction['facing'] == 'Up':
         if row[hero['point'][0]  - 1][hero['point'][1]] == 'X ':
-            change_response_phrase('damaged')
+            change_phrase('damaged')
             monster['health'] -= hero['attack']
         else:
-            change_response_phrase('missed')
+            change_phrase('missed')
             
-    elif response['last'] == 'Right':
+    elif direction['facing'] == 'Right':
         if row[hero['point'][0]][hero['point'][1]+1] == 'X ':
-            change_response_phrase('damaged')
+            change_phrase('damaged')
             monster['health'] -= hero['attack']
         else:
-            change_response_phrase('missed')
+            change_phrase('missed')
 
 def start():
 # starts game by creating to loops
@@ -174,9 +173,9 @@ def start():
             move(way)
         if monster['health'] == 0:
             row[monster['point'][0]][monster['point'][1]] = '0 '
-            change_response_phrase('deafated')
+            change_phrase('defeated')
         draw_grid()
-        print(response['phrase'])
+        print(phrase['last'])
 start()
 
 
